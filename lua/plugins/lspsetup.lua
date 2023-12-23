e7f7785bac8e7c7a5ec3a15c6df83787af179e19
Karthik Rathinavelu <r.karthik@outlook.com>
2023-12-24 03:20:52 +0530

    Fixing the missing nvim-cmp-lsp modules

--- lua/plugins/lspsetup.lua
+++ lua/plugins/lspsetup.lua
@@ -66,11 +66,14 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"creativenull/efmls-configs-nvim",
 	        "hrsh7th/nvim-cmp",
+	        "hrsh7th/cmp-buffer",
+	        "hrsh7th/cmp-path",
+	        "hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			require("neoconf").setup({})
 
-			-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 			local lspconfig = require("lspconfig")
 
 			local signs = { Error = " ", Warm = " ", Hint = "", Info = " " }
@@ -79,11 +82,11 @@ return {
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 			end
 
-			-- local capabilities = cmp_nvim_lsp.default_capabilities()
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 
 			-- configure lsp setup
 			lspconfig.lua_ls.setup({
-				-- capabilities = capabilities,
+				capabilities = capabilities,
 				on_attach = on_attach,
 				settings = {
 					Lua = {
@@ -102,7 +105,7 @@ return {
 
 			-- configure python setup
 			lspconfig.pyright.setup({
-				-- capabilities = capabilities,
+				capabilities = capabilities,
 				on_attach = on_attach,
 				settings = {
 					pyright = {
--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -1,6 +1,5 @@
 return {
 	"hrsh7th/nvim-cmp",
-    lazy = false,
 	dependencies = {
 		"onsails/lspkind.nvim",
 		{
